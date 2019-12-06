eb91a914d6cab2913bbecabc645f34b1af70d9a3
rparkar <rparkar@RENTLAPT0011>
2019-12-06 19:00:05 +0530

    Completed Rover Problem with Test cases

--- Rover/Rover.cs
+++ Rover/Rover.cs
@@ -4,23 +4,8 @@ using System.Text;
 
 namespace Rover
 {
-//    The Rover
 
-//You have a Rover that follows commands to move around on a plane surface.Locations on the surface are identified with an X-Y coordinate pair, and the Rover can face any one of 4 directions - North, South, East, West.
-
-//Commands are a sequence of characters - or a string - of the form
-
-//"LRLLMMMMLMMR" etc.So, given an initial location+direction combination (X, Y, D), and a command string C, you would want the Rover to process the command C and respond with an updated location+direction (X', Y', D').
-
-//Eg:
-
-//(0, 0, N) -- "MMM" -- (0, 3, N)
-
-//(0, 1, E) -- "MML" -- (2, 1, N)
-
-//(10, 5, N) -- "MMLMRMML" -- (?, ?, ?)
-
-    public class Rovers
+    class Rover
     {
         Location location = new Location();
         private string _commands;
@@ -58,7 +43,7 @@ namespace Rover
             location.UpdateDirection(direction);
         }
 
-        public string GetUpdatedLocation()
+        private string GetUpdatedLocation()
         {
             return location.GetUpdatedLocation();
         }
